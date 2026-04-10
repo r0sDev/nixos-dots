855a51f3433d169232c2c28fbea7b6959d4b4179
r0sDev <eliastello.dev@gmail.com>
2026-04-09 23:01:42 -0300

    Fixed modules folder & added nixos scripts from the modules

---
+++ nixos/modules/desktop.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+{
+  services.xserver = {
+    enable  = true;
+    displayManager.startx.enable     = true;
+    displayManager.defaultSession    = "none+i3";
+    windowManager.i3 = {
+      enable        = true;
+      extraPackages = with pkgs; [
+        i3status
+        i3lock
+        picom
+      ];
+    };
+  };
+
+  services.xserver.xkb = {
+    layout  = "es";
+    variant = "";
+  };
+
+  programs.light.enable = true;
+
+  fonts.packages = with pkgs; [
+    jetbrains-mono
+    nerd-fonts.jetbrains-mono
+    fira-code
+    fira-code-symbols
+    terminus_font
+  ];
+
+  fonts.fontconfig = {
+    enable        = true;
+    antialias     = true;
+    hinting.enable = true;
+    hinting.style  = "slight";
+    subpixel.rgba  = "rgb";
+  };
+}
---
+++ nixos/modules/packages.nix
@@ -0,0 +1,80 @@
+{    proton-vpn-cli, config, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+
+    # Shell
+    alacritty
+    neovim
+    git
+    eza
+    btop
+    fzf
+    cpufetch
+    acpi
+    ripgrep
+    fd
+    nodejs
+    gcc
+    fastfetch
+
+    # Lang
+    python3
+    cargo
+    jdk17
+
+    # i3 | Desktop utils
+    polybar
+    (polybar.override { pulseSupport = true; })
+    xorg.xrandr
+    brightnessctl
+    playerctl
+    pulseaudio       
+    pavucontrol
+    feh
+    xclip 
+    rofi
+    picom
+    dunst 
+    dmidecode
+    libreoffice
+
+    # Media
+    mpv
+    ffmpeg
+    wireplumber
+    alsa-utils
+
+    # Files 
+    yazi
+    pcmanfm
+    unzip
+    zip
+    xdg-utils
+    zathura
+    obsidian
+    taskwarrior2
+    steam-run
+    pkgs.ueberzugpp
+    p7zip
+    unrar
+
+    # Comunication
+    vesktop        
+
+    # It
+    nmap
+    anydesk
+    openssh
+    curl
+    wget
+    networkmanagerapplet
+    pkgs.cifs-utils
+    gh
+    speedtest-cli
+    tor
+
+    # For Microslop things
+    wine
+   
+];
+}
