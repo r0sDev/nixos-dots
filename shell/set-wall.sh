4ead44ba3544bd74399619ada8a5e56c8341aec0
r0sDev <eliastello.dev@gmail.com>
2026-05-13 17:44:26 -0300

    Added new forest wallpaper

--- shell/set-wall.sh
+++ shell/set-wall.sh
@@ -3,7 +3,7 @@
 hora=$(date +%H)
 
 WALLPAPER_DIA="/home/r0s/media/wall/bears-in-the-forest.jpg"
-WALLPAPER_NOCHE="/home/r0s/media/wall/bears-in-the-forest.jpg"
+WALLPAPER_NOCHE="/home/r0s/media/wall/forest.png"
 
 if ((hora >= 10 && hora < 16)); then
   feh --bg-fill "$WALLPAPER_DIA"
