49297054242975c87b38efdd1694f13de1f26d5e
r0sDev <eliastello.dev@gmail.com>
2026-06-09 21:41:01 -0300

    Renamed init.sh to wall-selector.sh to usage the script as an selector

---
+++ shell/mango/wall-selector.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+WALL_DIA_1="/home/r0s/media/wall/0355.jpg"
+WALL_DIA_2="/home/r0s/media/wall/0356.jpg"
+WALL_NOCHE_1="/home/r0s/media/wall/monocr_build.jpg"
+WALL_NOCHE_2="/home/r0s/media/wall/galen-crout.jpg"
+
+read -p "select wall: " selectedWall
+case $selectedWall in
+    one)
+    echo "selected wall 1"
+    swaybg -i "$WALL_DIA_1" -m fill &
+    ;;
+    two)
+    echo "selected wall 2"
+    swaybg -i "$WALL_DIA_2" -m fill &
+    ;;
+    three)
+    echo "selected wall 3"
+    swaybg -i "$WALL_NOCHE_1" -m fill & 
+    ;;
+    four)
+    echo "selected wall 4"
+    swaybg -i "$WALL_NOCHE_2" -m fill &
+    ;;
+    *)
+    echo "error on typing"
+    ;;
+esac
+
+
