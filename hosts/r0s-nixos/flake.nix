54123217663abaff76543903967c4e09a34aaa33
r0sDev <eliastello.dev@gmail.com>
2026-06-19 23:06:43 -0300

    Removed mangowm from flakes

--- hosts/r0s-nixos/flake.nix
+++ hosts/r0s-nixos/flake.nix
@@ -4,11 +4,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    mangowm = {
-      url = "github:mangowm/mango";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, mangowm, ... }@inputs:
