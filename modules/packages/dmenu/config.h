b53563943e81a08a3ef5ea74d3d73f22b97f88f1
r0sDev <eliastello.dev@gmail.com>
2026-07-09 22:49:53 -0300

    Added new fonts

--- modules/packages/dmenu/config.h
+++ modules/packages/dmenu/config.h
@@ -4,12 +4,12 @@
 static int topbar = 1;                    /* -b option; if 0, dmenu appears at bottom */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"IBM Plex Mono:size=11"
+	"CozetteVector:size=11"
 };
 static const char *prompt = NULL;         /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                fg          bg       */
-	[SchemeNorm]  = { "#bbbbbb",  "#111111" },
+	[SchemeNorm]  = { "#ffffff",  "#000000" },
 	[SchemeSel]   = { "#eeeeee",  "#333333" },
 	[SchemeOut]   = { "#000000",  "#00ffff" },
 };
