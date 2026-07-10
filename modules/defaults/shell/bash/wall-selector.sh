6ddc0f73d3f22b395729d5c7ab9ad29dc847c115
r0sDev <eliastello.dev@gmail.com>
2026-07-09 22:48:36 -0300

    Added new wall

--- modules/defaults/shell/bash/wall-selector.sh
+++ modules/defaults/shell/bash/wall-selector.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 WALL1="/home/r0s/media/wall/1.jpg"
-WALL2="/home/r0s/media/wall/forest.jpg"
+WALL2="/home/r0s/media/wall/forest.png"
 WALL3="/home/r0s/media/wall/galen-crout.jpg"
 WALL4="/home/r0s/media/wall/nasa-OLlj17tUZnU-unsplash.jpg"
 WALL5="/home/r0s/media/wall/wp8860785-nasa-4k-wallpapers.jpg"
 WALL6="/home/r0s/media/wall/kittensgame.jpg"
+WALL7="/home/r0s/media/wall/cities.jpg"
 
 read -p "select wall: " selectedWall
 case $selectedWall in
@@ -32,6 +33,10 @@ case $selectedWall in
     six|6)
     echo "selected wall 6"
     feh --bg-fill "$WALL6"
+    ;;
+    seven|7)
+    echo "selected wall 7"
+    feh --bg-fill "$WALL7"
     ;;
     *)
     echo "error on typing"
